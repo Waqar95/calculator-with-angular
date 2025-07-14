ea5086f90453559a61fc7080792c884e977262f0
waqar95 <waqar95ahmed@gmail.com>
2025-07-14 15:05:02 +0200

    install npm packages

---
+++ calculator-app/src/app/calculator-display/calculator-display.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator-display',
+  imports: [],
+  templateUrl: './calculator-display.html',
+  styleUrl: './calculator-display.css'
+})
+export class CalculatorDisplay {
+
+}
